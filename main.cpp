2a5083acc90fc17f306046c1454a85652fa9eb6d
Joaquin <joaquinlucascortez@gmail.com>
2024-05-29 00:41:17 -0300

    Implementacion del display del array (sin terminar).

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 //* Prototipos *//
 void cargarArray(char matrizPalabras[4][20]);
+void mostrarArray(char matrizPalabras[4][20]);
 
 int main (){
     int longFilas = 4;
@@ -18,7 +19,9 @@ int main (){
         cout << "  MENU  \n";
         cout << "(1) - Llenar arreglos de palabras\n";
         cout << "(2) - Mostrar arreglo cargado\n";
-        cout << "(3) - Encriptar palabras ingresadas\n";
+        cout << "(3) - Encriptar con codigo ASCII\n";
+        cout << "(4) - Encriptar con vocales desordenadas\n";
+        cout << "(5) - Encriptar con ambos metodos\n";
         cout << "(0) - Salir del programa\n";
         cin >> opcionUsuario;
 
@@ -39,7 +42,7 @@ int main (){
             break;
         case 2:
             if(cargado){
-
+                mostrarArray(matrizPalabras);
             }
             else{
                 cout << "No se ha cargado el arreglo de palabras.\n";
@@ -63,6 +66,7 @@ void cargarArray(char matrizPalabras[4][20]) {
     char letra;
     for (int filas = 0; filas < 4; filas++) {
         cout << "Ingrese la palabra " << filas + 1 << ": \n";
+        int columnas = 0;
         for (int columnas = 0; columnas < 20; columnas++) {
             cout << "Ingrese la letra de su palabra: \n";
             cin >> letra;
@@ -72,9 +76,22 @@ void cargarArray(char matrizPalabras[4][20]) {
                 matrizPalabras[filas][columnas] = letra;
             }
         }
+        matrizPalabras[filas][columnas] = '\0'; //? asigna  el caracter nulo al final de la palabra para darla como finalizada. evita signos raros.
     }
 }
 //MostrarArray
+void mostrarArray(char matrizPalabras[4][20]) {
+    for (int i = 0; i < 4; i++) {
+        cout << "Palabra " << i + 1 << ": ";
+        for (int j = 0; j < 20; j++) {
+            if (matrizPalabras[i][j] == '\0') { //?  si encuentra el caracter nulo, sale del bucle, la palabra ya termino, pasa a la siguiente.
+                break;  
+            }
+            cout << matrizPalabras[i][j];
+        }
+        cout << "\n";
+    }    
+}
 //TransformarVocales
 //TransformarASCII
 //TransformarAmbos
