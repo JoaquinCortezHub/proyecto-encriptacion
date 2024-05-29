ef870387d9ad04ccc6d26a4a185dac15feedd0bf
Joaquin <joaquinlucascortez@gmail.com>
2024-05-29 00:11:53 -0300

    Implementacion del cargado del array.

--- main.cpp
+++ main.cpp
@@ -4,49 +4,77 @@
 using namespace std;
 
 //* Prototipos *//
-void cargarArray(int matrizPalabras[4][20],int longFilas, int longColumnas);
+void cargarArray(char matrizPalabras[4][20]);
 
 int main (){
     int longFilas = 4;
     int longColumnas = 20;
-    int matrizPalabras [longFilas][longColumnas];
+    char matrizPalabras [4][20];
     int opcionUsuario;
+    bool cargado = false;
 
     cout << "Bienvenido al sistema de encriptacion\n";
-    cout << "  MENU  \n";
     do {
-        cout << "(1) - Cargar arreglo\n";   
-        cout << "(0) - Salir del programa\n";
-        cin >> opcionUsuario;
-        if (opcionUsuario == 0) {
-            cout << "Saliendo del sistema...\n";
-            break;
-        }
-    } while (opcionUsuario != 1 || opcionUsuario != 0);
-    do {
-        cout << "Arreglo cargado exitosamente, ingrese una opcion para operar: \n";
+        cout << "  MENU  \n";
+        cout << "(1) - Llenar arreglos de palabras\n";
         cout << "(2) - Mostrar arreglo cargado\n";
         cout << "(3) - Encriptar palabras ingresadas\n";
         cout << "(0) - Salir del programa\n";
         cin >> opcionUsuario;
 
-    } while (opcionUsuario == 0);
+        switch (opcionUsuario)
+        {
+        case 1:
+            cout << "INDICACIONES: \n";
+            cout << "Tiene la posibilidad de ingresar hasta 4 palabras de 20 caracteres como maximo.\n";
+            cout << "Carge una palabra a la vez.\n";
+            cout << "La palabra debe ser ingresada en minusculas.\n";
+            cout << "Al terminar la palabra, ingrese un punto (.) para finalizar.\n";
+            cout << "------------------------------------------------------------------------------------\n";
+            cargarArray(matrizPalabras);
+            cargado = true;
+            cout << "El arreglo ha sido cargado exitosamente.\n";
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+            if(cargado){
+
+            }
+            else{
+                cout << "No se ha cargado el arreglo de palabras.\n";
+                system("pause");
+                system("cls");
+            }
+            break;
+        case 0:
+            cout << "Saliendo del programa...\n";
+            break;
+        }
+    } while (opcionUsuario != 0);
     
-
-
-    //* Llamada de funciones *//
 
     return 0;
 }
 
 //* desarollo de funciones *//
 //cargarArray
-void cargarArray(int matrizPalabras[4][20], int longFilas, int longColumnas) {
-    for (int f = 0; f < longFilas; f++) {
-        
+void cargarArray(char matrizPalabras[4][20]) {
+    char letra;
+    for (int filas = 0; filas < 4; filas++) {
+        cout << "Ingrese la palabra " << filas + 1 << ": \n";
+        for (int columnas = 0; columnas < 20; columnas++) {
+            cout << "Ingrese la letra de su palabra: \n";
+            cin >> letra;
+            if (letra == '.') {
+                break;
+            } else {
+                matrizPalabras[filas][columnas] = letra;
+            }
+        }
     }
-    
 }
 //MostrarArray
 //TransformarVocales
 //TransformarASCII
+//TransformarAmbos
