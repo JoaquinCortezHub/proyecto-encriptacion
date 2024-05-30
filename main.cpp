159d074fe566766805ef1339c0e0c22e52801d0a
Joaquin <joaquinlucascortez@gmail.com>
2024-05-30 11:21:53 -0300

    Implementacion de enciptado por vocales desordenadas.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
-//palabra a codigo ASCII
-#include<iostream>
+// palabra a codigo ASCII
+#include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
 //* Prototipos *//
 void cargarArray(char matrizPalabras[4][20]);
 void mostrarArray(char matrizPalabras[4][20]);
+bool esVocal(char letra);
+void TransformarVocales(char matrizPalabras[4][20]);
 
-int main (){
+int main()
+{
     int longFilas = 4;
     int longColumnas = 20;
-    char matrizPalabras [4][20] = {};
+    char matrizPalabras[4][20] = {};
     int opcionUsuario;
     bool cargado = false;
 
     cout << "Bienvenido al sistema de encriptacion\n";
-    do {
+    do
+    {
         cout << "  MENU  \n";
         cout << "(1) - Llenar arreglos de palabras\n";
         cout << "(2) - Mostrar arreglo cargado\n";
@@ -41,14 +46,31 @@ int main (){
             system("cls");
             break;
         case 2:
-            if(cargado){
+            if (cargado)
+            {
                 mostrarArray(matrizPalabras);
                 cout << "-------------------\n";
                 cout << "Proceso finalizado.\n";
                 system("pause");
                 system("cls");
             }
-            else{
+            else
+            {
+                cout << "No se ha cargado el arreglo de palabras.\n";
+                system("pause");
+                system("cls");
+            }
+            break;
+        case 4:
+            if(cargado) {
+                TransformarVocales(matrizPalabras);
+                mostrarArray(matrizPalabras);
+                cout << "-------------------\n";
+                cout << "Proceso finalizado.\n";
+                system("pause");
+                system("cls");
+            }
+            else {
                 cout << "No se ha cargado el arreglo de palabras.\n";
                 system("pause");
                 system("cls");
@@ -60,50 +82,113 @@ int main (){
             break;
         }
     } while (opcionUsuario != 0);
-    
 
     return 0;
 }
 
 //* desarollo de funciones *//
-//cargarArray
-void cargarArray(char matrizPalabras[4][20]) {
+// cargarArray
+
+// TODO: Extraer la validación de la longitud de las palabras en un subporgrama diferente.
+void cargarArray(char matrizPalabras[4][20])
+{
     string palabra;
-    for (int filas = 0; filas < 4; filas++) {
+    for (int filas = 0; filas < 4; filas++)
+    {
         bool palabraValida = false;
-        while (!palabraValida) {
+        while (!palabraValida)
+        {
             cout << "Ingrese la palabra " << filas + 1 << ": \n";
             cin >> palabra;
             int longitudPalabra = palabra.length();
 
-            if(longitudPalabra > 20) {
-                cout << "La palabra ingresada es demasiado larga. Ingrese una palabra de maximo 20 caracteres.\n";    
+            if (longitudPalabra > 20)
+            {
+                cout << "La palabra ingresada es demasiado larga. Ingrese una palabra de maximo 20 caracteres.\n";
             }
-            else {
+            else
+            {
                 palabraValida = true;
-                for(int columnas = 0; columnas < longitudPalabra; columnas++) {
+                for (int columnas = 0; columnas < longitudPalabra; columnas++)
+                {
                     matrizPalabras[filas][columnas] = palabra[columnas];
                 }
             }
-            for(int columnas = longitudPalabra; columnas < 20; columnas++) {
-                matrizPalabras[filas][columnas] = '\0'; //? Asigna el caracter nulo a las posiciones vacias. previene simbolo y datos basuras en el display.
+            for (int columnas = longitudPalabra; columnas < 20; columnas++)
+            {
+                matrizPalabras[filas][columnas] = '\0'; //? Asigna el caracter nulo a las posiciones vacias. previene simbolos y datos basuras en el display.
             }
         }
     }
 }
-//MostrarArray
-void mostrarArray(char matrizPalabras[4][20]) {
-    for (int i = 0; i < 4; i++) {
-        cout << "Palabra " << i + 1 << ": ";
-        for (int j = 0; j < 20; j++) {
-            if (matrizPalabras[i][j] == '\0') { //?  si encuentra el caracter nulo, sale del bucle, la palabra ya termino, pasa a la siguiente.
-                break;  
+// MostrarArray
+void mostrarArray(char matrizPalabras[4][20])
+{
+    for (int filas = 0; filas < 4; filas++)
+    {
+        cout << "Palabra " << filas + 1 << ": ";
+        for (int columnas = 0; columnas < 20; columnas++)
+        {
+            if (matrizPalabras[filas][columnas] == '\0')
+            { //?  si encuentra el caracter nulo, sale del bucle, la palabra ya termino, pasa a la siguiente.
+                break;
             }
-            cout << matrizPalabras[i][j];
+            cout << matrizPalabras[filas][columnas];
         }
         cout << "\n";
-    }    
+    }
+}   
+// TransformarVocales
+bool esVocal(char letra){
+    char vocales[]= {'a', 'e', 'i', 'o', 'u'};
+    for (int i = 0; i < 5; i++) {
+        if (letra == vocales[i]){
+            return true;
+        }
+    }
+    return false;
 }
-//TransformarVocales
-//TransformarASCII
-//TransformarAmbos
+
+
+void TransformarVocales(char matrizPalabras[4][20]) {
+    char arrayVocales[5] = {'a', 'e', 'i', 'o', 'u'};
+    char arrayVocalesDesordenadas[5];
+
+    srand(time(NULL)); //? Inicializamos la semilla de generacion.
+
+    for(int i = 0; i < 5; i++) {
+        arrayVocalesDesordenadas[i] = arrayVocales[i];
+    }
+
+    for(int i = 0; i < 5; i++) { //? Mezcla las vocales cambiando la posicion de sus subindices.
+        int indiceRandom = rand() % 5;
+        char auxiliar = arrayVocalesDesordenadas[i];
+        arrayVocalesDesordenadas[i] = arrayVocalesDesordenadas[indiceRandom];
+        arrayVocalesDesordenadas[indiceRandom] = auxiliar;
+    }
+
+
+    for (int filas=0; filas < 4; filas++){
+        for (int columnas=0; columnas <20; columnas++){
+            char letraActual = matrizPalabras[filas][columnas];
+            if (letraActual=='\0') { //? si la palabra termina, deja de evaluar.
+                break;
+            }
+            if(esVocal(letraActual)){
+                for (int i = 0; i < 5; i++) {
+                    if(letraActual==arrayVocales[i]){
+                        matrizPalabras[filas][columnas] = arrayVocalesDesordenadas[i]; //? Asigna la vocal desordenada a la posicion actual de la palabra.
+                        break;
+                }
+                
+                }
+            }
+    }
+    }
+}
+// TransformarASCII
+// TransformarAmbos
+
+// A, E, I, O, U: asignarle numeros
+// rand() --> [4,2,1,0,3] <-- pasar
+//            {U,I,E,A,O}
